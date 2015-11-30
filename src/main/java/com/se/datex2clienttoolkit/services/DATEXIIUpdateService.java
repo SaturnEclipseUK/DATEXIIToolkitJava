2b353c3dbca37fb0fae235af6b30fd7fe762de04
Sam Roberts <samuel.roberts@saturneclipse.com>
2015-11-30 16:33:02 +0000

    Updated DATEXIIUpdateService to handle SOAP requests.

--- src/main/java/com/se/datex2clienttoolkit/services/DATEXIIUpdateService.java
+++ src/main/java/com/se/datex2clienttoolkit/services/DATEXIIUpdateService.java
@@ -8,6 +8,10 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -44,6 +48,7 @@ public class DATEXIIUpdateService {
 		}
 	}
 	
+	@SuppressWarnings("unchecked")
 	@Scheduled(fixedRate = 1000)
 	public void processDATEXIIUpdateXML(){
 		working = true;
@@ -55,13 +60,32 @@ public class DATEXIIUpdateService {
 			xml = messageQueue.poll();
 		}
 		while (xml != null){
-			try {
-				Unmarshaller unmarshaller = jaxbService.createUnmarshaller();
-				InputStream inputStream = new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8));
-				@SuppressWarnings("unchecked")
-				JAXBElement<D2LogicalModel> root = (JAXBElement<D2LogicalModel>) unmarshaller.unmarshal(inputStream);
-				D2LogicalModel d2lm = root.getValue();
+			try {			
+				byte[] bytes = xml.getBytes();
+				ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+						
+				XMLInputFactory xif = XMLInputFactory.newFactory();
+				XMLStreamReader xsr = null;
+				boolean foundBody = false;
+				xsr = xif.createXMLStreamReader(bais);					
+				while(!foundBody && xsr.hasNext()) {			
+					if(xsr.next() == XMLStreamConstants.START_ELEMENT &&
+						"http://schemas.xmlsoap.org/soap/envelope/".equals(xsr.getNamespaceURI()) &&
+						"Body".equals(xsr.getLocalName())) {
+							foundBody = true;
+							xsr.next();
+						}
+				}			
 				
+				JAXBElement<D2LogicalModel> root;
+				Unmarshaller unmarshaller = jaxbService.createUnmarshaller();
+				if(foundBody) {						
+					root = (JAXBElement<D2LogicalModel>)unmarshaller.unmarshal(xsr);
+				} else {
+					InputStream inputStream = new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8));
+					root = (JAXBElement<D2LogicalModel>)unmarshaller.unmarshal(inputStream);
+				}
+				D2LogicalModel d2lm = root.getValue();
 				FeedType feedType= getFeedType(d2lm);
 				DATEXIIProcessService datexiiProcessService = datexiiProcessServiceFactory.getDATEXIIProcessService(feedType);
 				if (datexiiProcessService != null){
@@ -70,7 +94,9 @@ public class DATEXIIUpdateService {
 				
 			} catch (JAXBException e) {
 				logger.error("Failed to process XML", e);
-			}
+			} catch (XMLStreamException e) {
+				logger.error("XMLStreamException", e);				
+			}	
 			synchronized (messageQueue){
 				xml = messageQueue.poll();
 			}
